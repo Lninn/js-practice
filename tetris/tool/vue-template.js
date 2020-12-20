f77da07b6625458fba5e120ac577bd4d606f92ae
Lninn <thl384458870@outlook.com>
2020-12-20 10:48:05 +0800

    update tool

--- tetris/tool/vue-template.js
+++ tetris/tool/vue-template.js
@@ -1,6 +1,6 @@
 import { UTILS } from "../utils";
 
-function initBrickData() {
+function initBrick() {
   const data = [],
     length = 5;
 
@@ -39,6 +39,8 @@ const BrickList = Vue.defineComponent({
   },
 });
 
+const DATA_KEY = "tetris";
+
 Vue.createApp({
   template: `
   <div class="tool-container">
@@ -50,7 +52,7 @@ Vue.createApp({
         </option>
       </select>
     </div>
-    <BrickList :dataSource="dataSource" @update="dataUpdate" />
+    <BrickList :dataSource="currentDataSource" @update="dataUpdate" />
     <div class="footer">
       <button @click="onSave">保存</button>
     </div>
@@ -60,45 +62,78 @@ Vue.createApp({
     BrickList,
   },
   data() {
+    const ret = this.initialize();
+    UTILS.log(ret);
+
     return {
-      alphabets: "I J L O S T Z".split(" "),
-      alphabet: "I",
-      dataSource: initBrickData(),
+      ...ret,
     };
   },
+  computed: {
+    currentDataSource: {
+      get() {
+        return this.dataSource[this.alphabet];
+      },
+      set(newValue) {
+        this.dataSource = newValue;
+      },
+    },
+  },
   methods: {
+    initialize() {
+      const defaultAlphabet = "I";
+      const alphabets = "I J L O S T Z".split(" ");
+
+      let dataSource;
+      let result = {
+        alphabets,
+        alphabet: defaultAlphabet,
+        dataSource: {},
+      };
+
+      let i;
+      i = localStorage.getItem(DATA_KEY);
+      if (i === null) {
+        result.dataSource = alphabets.reduce(function (accu, nextKey) {
+          accu[nextKey] = initBrick();
+
+          return accu;
+        }, {});
+
+        return result;
+      }
+
+      try {
+        i = JSON.parse(i);
+
+        dataSource = alphabets.reduce(function (accu, nextKey) {
+          if (i.hasOwnProperty(nextKey)) {
+            accu[nextKey] = i[nextKey];
+          } else {
+            accu[nextKey] = [];
+          }
+
+          return accu;
+        }, {});
+      } catch (error) {
+        i = {};
+      }
+
+      result.dataSource = dataSource;
+
+      return result;
+    },
     alphabetChange(e) {
       const i = e.target.value;
       this.alphabet = i;
     },
     dataUpdate(row, col, value) {
-      this.dataSource[row][col] = !value;
+      this.currentDataSource[row][col] = !value;
     },
     onSave() {
-      const KEY = "tetris";
+      this.dataSource[this.alphabet] = this.currentDataSource;
 
-      let i;
-      i = localStorage.getItem(KEY);
-      UTILS.log(i);
-
-      if (i === null) {
-        i = {
-          [this.alphabet]: this.dataSource,
-        };
-      } else {
-        try {
-          i = JSON.parse(i);
-        } catch (error) {
-          i = {};
-        }
-
-        i = {
-          ...i,
-          [this.alphabet]: this.dataSource,
-        };
-      }
-
-      localStorage.setItem(KEY, JSON.stringify(i));
+      localStorage.setItem(DATA_KEY, JSON.stringify(this.dataSource));
     },
   },
 }).mount("#tool");
