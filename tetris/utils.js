eb011295c7328353161e81e3ed2970c53eec65c2
HongLin Tian <thl384458870@outlook.com>
2021-02-03 02:46:49 +0000

    delete file

--- tetris/utils.js
+++ tetris/utils.js
@@ -42,3 +42,67 @@ export function get2DimMark(numOfRow, numOfCol) {
 
   return ret
 }
+
+export function getShapeSpace(shape) {
+  let list = [createNumbers(5), createNumbers(5, false)]
+  const ret = []
+  const END_STATUS = 3
+
+  function getIndex(status, row, col) {
+    if (status % 2 === 0) {
+      return [row, col]
+    } else {
+      return [col, row]
+    }
+  }
+
+  function start(status) {
+    let i = 0,
+      j = 0
+
+    if (status === 1 || status === 2) {
+      i = 1
+    }
+
+    let rows = list[i],
+      cols = list[j]
+
+    let pass = true,
+      count = 0
+    for (const row of rows) {
+      if (!pass) {
+        break
+      }
+
+      for (const col of cols) {
+        ;[i, j] = getIndex(status, row, col)
+
+        if (shape[i][j] === 1) {
+          pass = false
+          break
+        }
+      }
+
+      pass && count++
+    }
+
+    ret[status] = count
+
+    if (status === END_STATUS) {
+      return ret
+    } else {
+      return start(status + 1)
+    }
+  }
+
+  return start(0)
+}
+
+export const createNumbers = (length, dir = true) =>
+  Array.from({ length }).map((_, i) => {
+    if (dir) {
+      return i
+    } else {
+      return length - i - 1
+    }
+  })
