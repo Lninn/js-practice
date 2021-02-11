3f7a8ee3160e01ef316f23c2a4bcd86c420c3305
Lninn <thl384458870@outlook.com>
2021-02-11 13:38:41 +0800

    feat: add text

--- tetris/src/scene/end/index.js
+++ tetris/src/scene/end/index.js
@@ -1,4 +1,5 @@
 import Scene from '../Scene'
+import { CANVAS_HEIGHT, CANVAS_WIDTH } from '../../constant'
 
 export default class EndScene extends Scene {
   constructor(app) {
@@ -14,7 +15,10 @@ export default class EndScene extends Scene {
       app: { context },
     } = this
 
+    context.rect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    context.stroke()
+
     context.font = '12px serif'
-    context.fillText('游戏结束，刷新重新开始', 10, 50)
+    context.fillText('游戏结束，刷新重新开始', CANVAS_WIDTH / 2, 50)
   }
 }
